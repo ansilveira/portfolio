7a46dbdf82c11fb1c89a69b3cc11298fa65d4c3a
ansilveira <alissonns@hotmail.com>
2025-08-15 14:27:25 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
 const Sidebar = () => (
     <aside>
-        <img src="https://github.com/ansilveira.png" />
-        <Titulo fontSize={20}>Alisson Normando</Titulo>
+        <SidebarContainer>
+            <Avatar />
+            <Titulo fontSize={20}>Alisson Normando</Titulo>
+            <Paragrafo tipo="secundario" fontSize={16}>
+                ansilveira
+            </Paragrafo>
+            <Descricao tipo="principal" fontSize={12}>Engenheiro front-end</Descricao>
+            <BotaoTema>Trocar tema</BotaoTema>
+        </SidebarContainer>
     </aside>
 )
 
